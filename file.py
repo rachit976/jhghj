790041d13757ffd56d6599a67259c40787bc7ba3
rachit976 <mukeshsoni38@gmail.com>
2025-02-09 20:27:45 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,11 @@
+# printing a greeting message
+
+print("helllo world")
+
+#prnting my name 
+print("my name is rachit")
+
+#  printing a fun fact about myself 
+print("i love to do coding with angali mam")
+#  performing and printing a simple calculaton
+print("the result of 3 + 5 is:" , 3 + 5)
